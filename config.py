a5a14149daa6805c12faaf1a3ed77576720ae3cd
CHAPADOx7 <juliocesar.d.silvaporto@gmail.com>
2026-06-26 14:40:43 -0300

    config py

--- config.py
+++ config.py
@@ -13,11 +13,18 @@ DATABASE_PORT = os.environ.get('DB_PORT') or '3306'
 DATABASE_NAME = os.environ.get('DB_NAME', 'toplanches')
 DATABASE_QUERY = os.environ.get('DB_QUERY', '').strip()
 RESERVED_SYSTEM_SCHEMAS = {'sys', 'mysql', 'information_schema', 'performance_schema'}
+HAS_DB_COMPONENTS = any(os.environ.get(key) for key in ('DB_HOST', 'DB_USER', 'DB_NAME', 'DB_PORT', 'DB_PASSWORD'))
 
 if DATABASE_NAME.lower() in RESERVED_SYSTEM_SCHEMAS:
     DATABASE_NAME = 'toplanches'
 
-if DATABASE_URL:
+if HAS_DB_COMPONENTS:
+    encoded_user = quote_plus(DATABASE_USER)
+    encoded_password = quote_plus(DATABASE_PASSWORD)
+    encoded_name = quote_plus(DATABASE_NAME)
+    database_base_uri = f'mysql+pymysql://{encoded_user}:{encoded_password}@{DATABASE_HOST}:{DATABASE_PORT}/{encoded_name}'
+    DATABASE_URI = f'{database_base_uri}?{DATABASE_QUERY}' if DATABASE_QUERY else database_base_uri
+elif DATABASE_URL:
     DATABASE_URI = DATABASE_URL
 else:
     encoded_user = quote_plus(DATABASE_USER)
